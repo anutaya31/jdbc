d4c14c8927f6afd829daeb925fc95f67304e6e6f
Chaudhary <723311@1000810002446M.local>
2018-08-16 13:23:14 +1000

    Commiting all to local repo

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,39 @@
+package com.mycompany.app;
+
+import java.sql.*;
+
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+//step2 create  the connection object
+            Connection con = DriverManager
+                    .getConnection("jdbc:mysql://localhost:13306/testdb?useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "newuser", "Abc123");
+
+//step3 create the statement object
+            Statement stmt = con.createStatement();
+
+//step4 execute query
+
+            String strSelect = "select first_name, last_name from customers";
+            System.out.println("The SQL query is: " + strSelect); // Echo For debugging
+            System.out.println();
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            System.out.println("The records are:");
+            int rowCount = 0;
+            while (rset.next()) {
+                String firstname = rset.getString("first_name");
+                String lastName = rset.getString("last_name");
+                System.out.println(firstname + ", " + lastName);
+                ++rowCount;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
